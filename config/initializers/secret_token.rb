b29fc5724c8915f5b27ca7f5f97fdffbb50535ca
shade88 <shade88@yandex.ru>
2013-12-24 10:50:27 +0600

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Abn::Application.config.secret_key_base = 'fcd76a22bd9ff302b10f498fdc47eb16d36931bd7d249c10e2f5781c67cf5c13b6826fbab24adb7e0cd2e491c14e6776dc5a4ffe6e72eef71ec7b4c0cdfd5aaa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Abn::Application.config.session_store :cookie_store, key: '_abn_session'
